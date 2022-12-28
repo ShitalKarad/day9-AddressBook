55e43ef527c8d5baed2cf55f4c3ed6982509ef00
Shital <114717834+ShitalKarad@users.noreply.github.com>
2022-12-28 23:11:47 +0530

    Update AddressBook.java

--- com/brigelabz/AddressBook.java
+++ com/brigelabz/AddressBook.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    ArrayList<Contact> contactList = new ArrayList<>();
     public void addContact(){
         Contact contactPerson = new Contact();
         Scanner sc = new Scanner(System.in);
